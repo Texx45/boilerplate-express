3aa4dcf6b34535c4e217ce3b909b712328dd42b3
Texx45 <tcalderbank@hotmail.com>
2021-11-14 14:42:39 +0000

    completed lesson 1

--- myApp.js
+++ myApp.js
@@ -1,39 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const port = 5500;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
+app.listen(port, () => {
+  console.log("Hello World");
+  console.log(`server is running on port ${port}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
